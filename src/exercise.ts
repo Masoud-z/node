d076b8c1b0850de11a2d68de130521ab33bef35a
Masoud-z <masoud.zeinalabedini@gmail.com>
2025-04-27 16:56:09 +0330

    Add validation

--- src/exercise.ts
+++ src/exercise.ts
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
+interface ICourse extends mongoose.Document {
+  name: string;
+  author: string;
+  tags: string[];
+  date: Date;
+  published: boolean;
+  price: number;
+}
+
 const courses = [
   "nodejs",
   "javascript",
@@ -33,11 +42,26 @@ mongoose
   });
 
 const courseSchema = new mongoose.Schema({
-  name: String,
-  author: String,
+  name: {
+    type: String,
+    required: Boolean,
+    validate: {
+      validator: function (v: string) {
+        return !!v && v.length > 3;
+      },
+      message: "Name should be at least 3 characters long",
+    },
+  },
+  author: { type: String, required: Boolean },
   tags: [String],
   date: { type: Date, default: Date.now },
   published: Boolean,
+  price: {
+    type: Number,
+    required: function (this: ICourse) {
+      return this.published;
+    },
+  },
 });
 
 const Course = mongoose.model("Course", courseSchema);
@@ -46,36 +70,55 @@ function createCourse(
   name: string,
   author: string,
   tags: string[],
-  published: boolean
+  published: boolean,
+  price: number
 ) {
   const course = new Course({
     name,
     author,
     tags,
     published,
+    price,
   });
 
-  return course.save();
+  return course
+    .save()
+    .then((course) => course)
+    .catch((err) => {
+      if (err instanceof Error) {
+        console.error("Error creating course: ", err.message);
+      } else {
+        console.error("Error creating course: ", err);
+      }
+    });
 }
+
+createCourse("N", "Masoud", ["nodejs", "javascript"], true, 900).then(
+  (course) => {
+    console.log("createCourse2: ", course);
+  }
+);
 
 // for (let i = 1; i < 100; i++) {
 //   const randomIndex = Math.floor(Math.random() * 20);
 //   const randomBinary = Math.floor(Math.random() * 2);
+//   const randomPrice = Math.floor(Math.random() * 1000) + 1;
 
 //   createCourse(
 //     `${courses[randomIndex]} Course ${i}`,
 //     "Masoud",
 //     [`${i}`, `${i} ${i} ${i} ${i}`],
-//     !!randomBinary
+//     !!randomBinary,
+//     randomPrice
 //   ).then((course) => {
 //     console.log("Course created: ", course);
 //   });
 // }
 
 function getCourses() {
-  return Course.find({ published: true })
+  return Course.find({ published: true, price: { $gte: 900, $lt: 950 } })
     .sort({ name: 1 })
-    .select({ name: 1, author: 1 });
+    .select({ name: 1, price: 1 });
 }
 
 getCourses().then((courses) => {
